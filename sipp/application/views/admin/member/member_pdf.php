30ab2eac7b5760c9683d827c09b6a90be90adbff
achyar10 <achyar.anshorie@gmail.com>
2015-12-05 23:37:31 +0700

    Revert "paragraf"
    This reverts commit 3be47ad4c616d2871adb3d3ae63108365def4795.

--- sipp/application/views/admin/member/member_pdf.php
+++ sipp/application/views/admin/member/member_pdf.php
@@ -1,30 +1,20 @@
 <html>
     <head>
-    <style type="text/css">
-   .upper { text-transform: uppercase; }
-   .lower { text-transform: lowercase; }
-   .cap   { text-transform: capitalize; }
-   .small { font-variant:   small-caps; }
-</style>
     </head>
     <body>
-    <div style="padding:0px 0px;">
+        <div style="padding:0px 0px;">
 
-            <div align="center">
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <h3><strong><u> SURAT KETERANGAN MAGANG KERJA</u></strong></h3>              
-          0<?php echo substr($member['member_nip'],3) ?>/SAT-HRA/CLS2/<?php echo pretty_date(date('Y-m-d'), 'm',false)?>/<?php echo pretty_date(date('Y-m-d'), 'Y',false)?>              </p>
-            </div>
-			<br>
-            <p align="justify"> Yang bertanda tangan dibawah ini,</p>
+            <strong><u><h4 style="padding-bottom: 0px   " align="center">SURAT KETERANGAN MAGANG KERJA</h4></u></strong>
+            <h5 style="padding-top: 0px" align="center">008/SAT-HRA/CLS2/XI/2015</h5>
+
+            <p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Yang bertanda tangan dibawah ini :</p>
 
             <table border="0">
                 <tbody>
                     <tr>
-                        <td width="121">Nama</td>
-                        <td width="10">:</td>
-                        <td width="526">TATI NURHAYATI</td>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td>TATI NUTHAYATI</td>
                     </tr>
                     <tr>
                         <td>Jabatan</td>
@@ -44,18 +34,18 @@
                     <tr>
                         <td></td>
                         <td></td>
-                        <td>Bogor Kode Pos 16820 </td>
+                        <td>Bogor 16820 </td>
                     </tr>
                 </tbody>
             </table>
-            
-            <p align="justify"> Menerangkan bahwa,</p>
+            <br>
+            <p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Menerangkan bahwa :</p>
              <table border="0">
                 <tbody>
                     <tr>
-                        <td width="120">Nama</td>
-                        <td width="11">:</td>
-                        <td width="524"> <span class="upper"><?php echo $member['member_full_name'] ?></span></td>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td><?php echo $member['member_full_name'] ?></td>
                     </tr>
                     <tr>
                         <td>Asal Sekolah</td>
@@ -70,41 +60,23 @@
                 </tbody>
              </table>
             <br>
-            <p align="justify">Bahwa yang bersangkutan telah melaksanakan kegiatan magang kerja di PT. Sumber Alfaria Trijaya, Tbk magang kerja tersebut telah dilaksanakan selama 3 bulan, yaitu mulai tanggal <?php echo pretty_date( $member['member_input_date'],'d F Y',false) ?> s/d tanggal <?php echo pretty_date( $member['member_last_update'],'d F Y',false) ?>.</p>
+            <p align="justify">Bahwa yang bersankutan telah melaksanakan kegiatan magang kerja di PT. Sumber Alfaria Trijaya, Tbk, magang kerja tersebut telah dilaksanakan selama 3 bulan, yaitu mulai tanggal <?php echo pretty_date( $member['member_input_date'],'d F Y',false) ?> s/d tanggal <?php echo pretty_date( $member['member_last_update'],'d F Y',false) ?>.</p>
             
             <p align="justify">Selama bekerja, yang bersangkutan telah menunjukan presetasi dan dedikasi yang baik, Manajemen mengucapkan terima kasih atas partisipasinya selama magang di perusahaan.</p>
             
             <p align="justify">Demikian Surat Keterangan Magang ini dibuat dengan sebenarnya, untuk dapat dipergunakan seperlunya.</p>
-            
-            <br><br>            
-            <table border="0">
-                <tbody>
-                    <tr>
-                        <td>Cileungsi, <?php echo pretty_date(date('Y-m-d'), 'd F Y',false)?> </td>
-                        <td></td>                        
-                    </tr>
-                    <tr>
-                        <td>PT. Sumber Alfaria Trijaya, Tbk</td>
-                        <td></td>                        
-                    </tr>
-              </tbody></table>             
-           
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <table border="0">
-                <tbody>
-                    <tr>
-                        <td><u><strong>TATI NURHAYATI</strong></u></td>
-                        <td></td>                        
-                    </tr>
-                    <tr>
-                        <td><em><strong>People Development Manager</strong></em></td>
-                        <td></td>                        
-                    </tr>
-                  </tbody></table>
-    </div>
+
+            <br>
+            <strong><i><h4 align="justify">Cileungsi, <?php echo pretty_date(date('Y-m-d'), 'd F Y',false)?></h4></i></strong>
+            <strong><i align="justify">PT. Sumber Alfaria Trijaya, Tbk</i></strong>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <strong><u align="justify">TATI NURHAYATI</u></strong>
+            <strong><p><i align="justify">People Development Manager</i></p></strong>
+        </div>
     </body>
 </html>
